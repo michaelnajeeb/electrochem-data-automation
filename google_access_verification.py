ce8370b99771c341c7bd0b5c8e39c8f0c500a08d
Michael Najeeb <michaeleliasnajeeb@gmail.com>
2025-07-29 18:42:57 -0700

    google_access_verification.py file updated

--- google_access_verification.py
+++ google_access_verification.py
@@ -1,7 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# specify location
+# specify name of your specific google sheet
 google_sheet_name = "Cell Tracker v3 Sandbox"
 
 # authorize 
@@ -12,7 +12,7 @@ client = gspread.authorize(creds)
 # verify
 
 sheet = client.open(google_sheet_name)
-print(f"{google_sheet_name} has been found. The file electrochemical_data_automation.py can be run.")
+print(f"{google_sheet_name} has been found.")
 
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
@@ -27,15 +27,15 @@ gauth = GoogleAuth()
 gauth.credentials = creds
 drive = GoogleDrive(gauth)
 
-# create folder IDs
+# create folder variables and input your specific folder IDs
 
-raw_data_folder_ID = "1qzZvrcHyIsTdPfekkZsCuHJf59CBaA-b"
-combined_data_folder_ID = "1JRjAGeCW1hqFmUqVBA6HdP6-sIvhkxW6"
-figures_folder_ID = "1ACa1FkDjTjUaN-trmbZgCangWDWxvqF6"
+raw_data_folder_ID = "folder ID goes here"
+combined_data_folder_ID = "folder ID goes here"
+figures_folder_ID = "folder ID goes here"
 
 # list files found in the folders
 
-print("Files in Cell Cycler Data (.txt) Sandbox:")
+print("Files found:")
 file_list = drive.ListFile({'q' : f"'{raw_data_folder_ID}' in parents and trashed=false"}).GetList()
 for file in file_list:
     print(f"- {file['title']} ({file['id']})")
